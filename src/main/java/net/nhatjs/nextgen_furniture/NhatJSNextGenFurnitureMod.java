e65133c8e3b0fd8883b7c343d2bbabaab7d2a8c6
NhatJS <nhatjsouthfall7@gmail.com>
2025-10-21 15:53:44 +0700

    update features to 1.21.4

--- src/main/java/net/nhatjs/nextgen_furniture/NhatJSNextGenFurnitureMod.java
+++ src/main/java/net/nhatjs/nextgen_furniture/NhatJSNextGenFurnitureMod.java
@@ -69,7 +69,7 @@ public class NhatJSNextGenFurnitureMod {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = NhatJSNextGenFurnitureMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = NhatJSNextGenFurnitureMod.MOD_ID, value = Dist.CLIENT)
     static class ClientModEvents {
         @SubscribeEvent
         static void onClientSetup(FMLClientSetupEvent event) {
